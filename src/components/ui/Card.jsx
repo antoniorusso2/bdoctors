85b1e3d6c779c23e9fa18c7fd8f8856f06c870d5
giovanni-arduini <gionnibigudd@gmail.com>
2025-01-22 12:23:54 +0100

    ADD Card style

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -6,8 +6,8 @@ export default function Card({ doctor }) {
   const voteStars = [1, 2, 3, 4, 5];
   return (
     <>
-      <Link to={`doctors/${doctor.id}`} className="card main-card p-3">
-        <div className="card-body">
+      <Link to={`doctors/${doctor.id}`} className="card border-0 main-card p-3">
+        <div className="card-body card-body-background">
           <p className="card-title">
             {doctor.first_name}
             <span> </span>
@@ -15,7 +15,10 @@ export default function Card({ doctor }) {
           </p>
           <p className="mb-3">Specialista in {doctor.specialization}</p>
 
-          <div>
+          <div
+            className="mb-4
+          "
+          >
             {voteStars.map((n, i) => {
               return n <= doctor.avg_vote ? (
                 <StarSolid key={i} className="starAvg text-warning"></StarSolid>
@@ -23,6 +26,13 @@ export default function Card({ doctor }) {
                 <StarIcon key={i} className="starAvg text-warning"></StarIcon>
               );
             })}
+          </div>
+          <div className="d-flex justify-content-center ">
+            <div className="btn-contact-container ">
+              <a href="#" className=" btn contact-btn">
+                Contatta lo specialista
+              </a>
+            </div>
           </div>
         </div>
       </Link>
